68a62c952d2909fcd8d36c35784586e70bfb2878
Matias Halles <matias.halles@gmail.com>
2020-04-29 19:05:58 -0400

    Move method from module file into `UserEntity.php`

--- src/UserEntity.php
+++ src/UserEntity.php
@@ -44,4 +44,31 @@ class UserEntity extends User
         }
         parent::preSave($storage);
     }
+
+    protected function special_group() {
+
+        // Adds JSE Access when Special Group flag is turned on
+        $_original = $this->original->get('special_group')->getValue()[0]['value'];
+        $_new = $this->get('special_group')->getValue()[0]['value'];
+
+        if ($_original !== $_new) {
+            // Set access flag to true only if
+            if($_new === 1) {
+                $this->set('jse_access', 1);
+            }
+
+            // Toggles user access to content group
+            /** @var Group $group */
+            $group =  \Drupal::entityQuery('group')
+                ->condition('type', 'dpc_module_group_type')
+                ->accessCheck(false)
+                ->execute();
+
+            if($this->get('jse_access')->getValue()[0]['value']) {
+                $group->addMember($this);
+            } else {
+                $group->removeMember($this);
+            }
+        }
+    }
 }
