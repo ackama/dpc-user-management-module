dffce352bfafab97cfe2e62900cb8d190bd44d6e
Matias Halles <matias.halles@gmail.com>
2020-05-12 11:54:12 -0400

    Setup MailHog for Development

--- dev/config/services/drupal/root/setup.sh
+++ dev/config/services/drupal/root/setup.sh
@@ -44,6 +44,14 @@ drush --yes pm:enable admin_toolbar_search
 # Enable our module's Drupal dependencies (they are installed via composer)
 drush --yes pm:enable group
 
+# Enbale phpmail smtp and mailsystem
+# Configures both to route emails to mailhog docker service
+drush --yes pm:enable phpmailer_smtp mailsystem
+drush config:set --yes mailsystem.settings defaults.sender phpmailer_smtp
+drush config:set --yes mailsystem.settings defaults.formatter phpmailer_smtp
+drush config:set --yes phpmailer_smtp.settings smtp_host mailhog
+drush config:set --yes phpmailer_smtp.settings smtp_port 1025
+
 # Rebuild drupal cache after installing Drupal and enabling plugins. Cache
 # rebuilds sometimes fail the first time for no apparent reason so we run with
 # `|| true` to swallow that failure
