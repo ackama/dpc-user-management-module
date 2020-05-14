0d944d11b8065e6b0052dff7fe655da3f46a5898
Matias Halles <matias.halles@gmail.com>
2020-05-13 21:21:53 -0400

    Makes reminder block editable

--- src/Plugin/Block/AccessReminderBlock.php
+++ src/Plugin/Block/AccessReminderBlock.php
@@ -45,11 +45,21 @@ class AccessReminderBlock extends BlockBase implements BlockPluginInterface{
     public static $_div_class = 'dpc-user-email-reminder';
 
     /**
+     * @var string
+     */
+    public $field_id_content = 'content';
+
+    /**
      * {@inheritdoc}
      */
     public function build() {
+
+        $config = $this->getConfiguration();
+
+        $content = !empty($config[$this->field_id_content]) ? $config[$this->field_id_content] : self::$_default_text;
+
         // Formats text to include user edit link
-        $text = $this->t(sprintf(self::$_default_text, '/user/' . \Drupal::currentUser()->id() . '/edit'));
+        $text = $this->t(sprintf($content, '/user/' . \Drupal::currentUser()->id() . '/edit'));
 
         return [
             '#markup' => sprintf('<div id="%s" class="%s">%s</div>', self::$_div_id, self::$_div_class, $text),
@@ -87,11 +97,11 @@ class AccessReminderBlock extends BlockBase implements BlockPluginInterface{
 
         $config = $this->getConfiguration();
 
-        $form['access_reminder_block'] = [
+        $form[$this->field_id_content] = [
             '#type' => 'textfield',
-            '#title' => $this->t('Reminder Text'),
-            '#description' => $this->t('Text that users who don\'t yet have access to the content will see. Use <b>%s</b> to insert the user\'s profile link. ie. <a href="%s">My Profile</a>'),
-            '#default_value' => isset($config[self::$_id]) ? $config[self::$_id] : self::$_default_text,
+            '#title' => $this->t('Text for Access Reminder'),
+            '#description' => $this->t('Text that users who don\'t yet have access to the content will see. Use <b>%s</b> to insert the current user\'s profile link. ie. &lt;a href="%s"&gt;Link to Profile&lt;/a&gt;.'),
+            '#default_value' => !empty($config[$this->field_id_content]) ? $config[$this->field_id_content] : self::$_default_text
         ];
 
         return $form;
@@ -102,7 +112,6 @@ class AccessReminderBlock extends BlockBase implements BlockPluginInterface{
      */
     public function blockSubmit($form, FormStateInterface $form_state) {
         parent::blockSubmit($form, $form_state);
-        $values = $form_state->getValues();
-        $this->configuration[self::$_id] = $form_state->getValue(self::$_id);
+        $this->configuration[$this->field_id_content] = $form_state->getValue($this->field_id_content);
     }
 }
