1595baf92737bd05fe691e310bbdcf24ad7ad037
Matias Halles <matias.halles@gmail.com>
2020-05-19 02:05:44 -0400

    Use queue provided by controller instead of loading a new one

--- src/Controller/EventsLogController.php
+++ src/Controller/EventsLogController.php
@@ -270,8 +270,7 @@ class EventsLogController extends ControllerBase
             // If there are groups in the removed key of the response, attempt sending emails
             if (!$user->inAccessGroup() && key_exists($user->accessGroup()->id(), $result['removed'])) {
                 // Queues sending notifications
-                $queue = \Drupal::queue('notify_user_task');
-                $queue->createItem([
+                $this->queue()->createItem([
                     'user_id' => $user->id(),
                     'removed' => $result['removed']
                 ]);
