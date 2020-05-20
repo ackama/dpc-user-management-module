16100ee7ab4d449269c2d92e73f242e10392331d
Matias Halles <matias.halles@gmail.com>
2020-05-20 18:33:05 -0400

    Move processing code for group memberships from Task to Controller and use reference in Task

--- src/Plugin/QueueWorker/GroupMembershipUpdateTask.php
+++ src/Plugin/QueueWorker/GroupMembershipUpdateTask.php
@@ -2,9 +2,7 @@
 
 namespace Drupal\dpc_user_management\Plugin\QueueWorker;
 use Drupal\Core\Queue\QueueWorkerBase;
-use Drupal\dpc_user_management\Traits\HandlesEmailDomainGroupMembership;
-use Drupal\group\Entity\Group;
-use Drupal\user\Entity\User;
+use Drupal\dpc_user_management\Controller\UserEntityController;
 
 /**
  * Processes Entity Update Tasks for My Module.
@@ -16,7 +14,6 @@ use Drupal\user\Entity\User;
  * )
  */
 class GroupMembershipUpdateTask extends QueueWorkerBase {
-    use HandlesEmailDomainGroupMembership;
 
     /**
      * Works on a single queue item.
@@ -45,49 +42,7 @@ class GroupMembershipUpdateTask extends QueueWorkerBase {
      */
     public function processItem($data)
     {
-        /** @var Group $group */
-        $group_id = $data['group'];
-        $group =  Group::load($group_id);
-
-        // check if existing users can be added to the group
-        $query = \Drupal::entityQuery('user');
-
-        $domains = $group->field_email_domain->getValue();
-
-        $field_email_orgroup = $query->orConditionGroup();
-        $mail_orgroup = $query->orConditionGroup();
-        $base_orgroup = $query->orConditionGroup();
-
-        foreach ($domains as $domain) {
-            $field_email_orgroup->condition('field_email_addresses', '%' . $domain['value'], 'like');
-            $mail_orgroup->condition('mail', '%' . $domain['value'], 'like');
-        }
-
-        $base_orgroup->condition($field_email_orgroup);
-        $base_orgroup->condition($mail_orgroup);
-        $query->condition($base_orgroup);
-
-        $uids = $query->execute();
-
-        $users = User::loadMultiple($uids);
-        $domains = array_column($domains, 'value');
-
-        foreach ($users as $user) {
-            if ($group->getMember($user)) {
-                continue;
-            }
-
-            $addresses = $user->field_email_addresses->getValue();
-            if (empty($addresses)) {
-                self::addUserToGroup($user, $group);
-
-                continue;
-            }
-            foreach ($addresses as $key => $email) {
-                if (in_array(explode('@', $email['value'])[1], $domains) && $email['status'] === 'verified') {
-                    self::addUserToGroup($user, $group);
-                }
-            }
-        }
+        $controller = new UserEntityController();
+        $controller->processGroupMemberships($data);
     }
 }
