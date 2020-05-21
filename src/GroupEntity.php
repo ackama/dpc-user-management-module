48a5d92e60d720eb0307dce9429dd856ae61f00d
Matias Halles <matias.halles@gmail.com>
2020-05-21 01:38:13 -0400

    Remember removed domains in order to process after

--- src/GroupEntity.php
+++ src/GroupEntity.php
@@ -9,18 +9,47 @@ use Drupal\group\Entity\Group;
 class GroupEntity extends Group
 {
 
-//    public function flagDirtyDomains() {
-//
-//    }
-//
-//    public function preSave(EntityStorageInterface $storage)
-//    {
-//        parent::preSave($storage);
-//
-//        if ($this->getGroupType()->id() == UserEntity::$group_type_email_domain_id) {
-//            $this->flagDomainsForDeletion();
-//        }
-//    }
+    /**
+     * Saves removed domains in state key in order to process them postSave
+     * It does this by looking at the dirty field and the new one pre save.
+     * This pushes the domain values into an array of domains that is not cleaned
+     * until the actual reprocessing of user memberships. Long story short,
+     * getting these from guessing from DB data looks expensive.
+     *
+     * The result is stored in the State store.
+     * IMPORTANT: values need to be rechecked before removing before doing anything
+     */
+    public function rememberDomainsPreSave() {
+
+        // Gets original field data
+        $original = array_map(function($x){
+            return $x['value'];
+        },$this->original->get('field_email_domain')->getValue());
+
+        // Gets current object data
+        $clean = array_map(function($x){
+            return $x['value'];
+        },$this->get('field_email_domain')->getValue());
+
+        // Gets domains to be removed
+        $removed = array_diff($original, $clean);
+
+        if(!count($removed)) {
+            return;
+        }
+
+        $domains = \Drupal::state()->get('dpc_group_domains_remove',[]);
+        \Drupal::state()->set('dpc_group_domains_remove', array_merge($domains, $removed));
+    }
+
+    public function preSave(EntityStorageInterface $storage)
+    {
+        parent::preSave($storage);
+
+        if ($this->getGroupType()->id() == UserEntity::$group_type_email_domain_id) {
+            $this->rememberDomainsPreSave();
+        }
+    }
 
     /**
      * @param EntityStorageInterface $storage
