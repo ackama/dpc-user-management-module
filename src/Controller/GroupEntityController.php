30d63273d8c119b8e6de373e1860391964b88dd5
Matias Halles <matias.halles@gmail.com>
2020-05-21 00:15:53 -0400

    Move automatic processing of memberships into GroupEntity

--- src/Controller/GroupEntityController.php
+++ src/Controller/GroupEntityController.php
@@ -3,62 +3,10 @@ namespace Drupal\dpc_user_management\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\dpc_user_management\Plugin\QueueWorker\GroupMembershipUpdateTask;
-use Drupal\group\Entity\Group;
+use Drupal\dpc_user_management\GroupEntity as Group;
 
 class GroupEntityController extends ControllerBase
 {
-    /**
-     * @param $data
-     * @throws \Exception
-     */
-    public function processGroupMemberships($data)
-    {
-        /** @var Group $group */
-        $group_id = $data['group'];
-        $group =  Group::load($group_id);
-
-        // check if existing users can be added to the group
-        $query = \Drupal::entityQuery('user');
-
-        $domains = $group->field_email_domain->getValue();
-
-        $field_email_orgroup = $query->orConditionGroup();
-        $mail_orgroup = $query->orConditionGroup();
-        $base_orgroup = $query->orConditionGroup();
-
-        foreach ($domains as $domain) {
-            $field_email_orgroup->condition('field_email_addresses', '%' . $domain['value'], 'like');
-            $mail_orgroup->condition('mail', '%' . $domain['value'], 'like');
-        }
-
-        $base_orgroup->condition($field_email_orgroup);
-        $base_orgroup->condition($mail_orgroup);
-        $query->condition($base_orgroup);
-
-        $uids = $query->execute();
-
-        $users = \Drupal\user\Entity\User::loadMultiple($uids);
-        $domains = array_column($domains, 'value');
-
-        foreach ($users as $user) {
-            if ($group->getMember($user)) {
-                continue;
-            }
-
-            $addresses = $user->field_email_addresses->getValue();
-            if (empty($addresses)) {
-                self::addUserToGroup($user, $group);
-
-                continue;
-            }
-            foreach ($addresses as $key => $email) {
-                if (in_array(explode('@', $email['value'])[1], $domains) && $email['status'] === 'verified') {
-                    self::addUserToGroup($user, $group);
-                }
-            }
-        }
-    }
-
     /**
      * Runs the group_membership_update_task queue
      */
--- src/GroupEntity.php
+++ src/GroupEntity.php
@@ -1,12 +1,27 @@
 <?php
 namespace Drupal\dpc_user_management;
 
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\dpc_user_management\Controller\GroupEntityController;
 use Drupal\group\Entity\Group;
 
 class GroupEntity extends Group
 {
+
+//    public function flagDirtyDomains() {
+//
+//    }
+//
+//    public function preSave(EntityStorageInterface $storage)
+//    {
+//        parent::preSave($storage);
+//
+//        if ($this->getGroupType()->id() == UserEntity::$group_type_email_domain_id) {
+//            $this->flagDomainsForDeletion();
+//        }
+//    }
+
     /**
      * @param EntityStorageInterface $storage
      *
@@ -17,12 +32,81 @@ class GroupEntity extends Group
         parent::postSave($storage, $update);
 
         if ($this->getGroupType()->id() == UserEntity::$group_type_email_domain_id) {
-            (new GroupEntityController())->processGroupMemberships(['group' =>  $this->id()]);
+            $this->processGroupMemberships();
         }
     }
 
+    /**
+     * @return mixed
+     */
     public function getName()
     {
         return $this->get('label')->getValue()[0]['value'];
     }
+
+    /**
+     * @return array
+     */
+    public function domains() {
+        return array_map(function ($x) {
+            return $x['value'];
+        }, $this->get('field_email_domain')->getValue());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function processGroupMemberships()
+    {
+        $this->discoverMembers();
+    }
+
+    /**
+     * Discover Members who have emails belonging to the group's domain list
+     *
+     * @return EntityInterface[]|UserEntity[]
+     */
+    public function discoverMembers() {
+        $query = \Drupal::entityQuery('user');
+
+        $field_email_orgroup = $query->orConditionGroup();
+        $mail_orgroup = $query->orConditionGroup();
+        $base_orgroup = $query->orConditionGroup();
+
+        foreach ($this->domains() as $domain) {
+            $field_email_orgroup->condition(
+                $query->andConditionGroup()
+                    ->condition('field_email_addresses.value', '%' . $domain, 'like')
+                    ->condition('field_email_addresses.status', 'verified')
+            );
+            $mail_orgroup->condition('mail', '%' . $domain, 'like');
+        }
+
+        $base_orgroup->condition($field_email_orgroup);
+        $base_orgroup->condition($mail_orgroup);
+        $query->condition($base_orgroup);
+
+        $uids = $query->execute();
+
+        return \Drupal\user\Entity\User::loadMultiple($uids);
+    }
+
+    /**
+     * @param $users UserEntity[]
+     * @throws \Exception
+     */
+    protected function addMembers($users) {
+        foreach ($users as $user) {
+            if ($this->getMember($user)) {
+                continue;
+            }
+
+            $user->addToGroup($this);
+        }
+    }
+
+    protected function removeExistingMembers() {
+
+    }
+
 }
