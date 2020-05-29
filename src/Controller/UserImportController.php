5e0837b70b2ebf26ace4e5fc7b07cf25b5945ae8
Matias Halles <matias.halles@gmail.com>
2020-05-28 22:35:23 -0400

    Get record in array form instead of object

--- src/Controller/UserImportController.php
+++ src/Controller/UserImportController.php
@@ -159,7 +159,7 @@ class UserImportController extends ControllerBase
             ->orderBy('id')
             ->condition('id',$id)
             ->execute()
-            ->fetchObject();
+            ->fetchAssoc();
     }
 
     public function getRecordsCount()
